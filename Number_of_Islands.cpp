223a103b9082fe00427d3fb9e58cb12fecadc127
MAPLE0934 <swapneelbose70@gmail.com>
2025-02-25 15:04:39 +0530

    doing the explore function

--- Number_of_Islands.cpp
+++ Number_of_Islands.cpp
@@ -5,7 +5,33 @@
 
 using namespace std;
 
-int main(){
+bool explore(vector<vector<char>>& islands, int row, int col, unordered_set<string>& visited){
+    bool rowBounds = 0 <= row && row < islands.size();
+    bool colBounds = 0 <= col && row < islands[0].size();
+
+    if(!rowBounds || !colBounds) return false;
+    if(islands[row][col] == 'W') return false;
+    string pos = to_string(row) + ',' + to_string(col);
     
+    if(visited.find(pos) != visited.end()) return false;
+
+    
+}
+int main(){
+    vector<vector<char>> islands;
+    unordered_set<string> visited;
+    int count = 0;
+    islands = {{'W', 'L', 'W', 'W', 'W'},
+               {'W', 'L', 'W', 'W', 'W'},
+               {'W', 'W', 'W', 'L', 'W'},
+               {'W', 'W', 'L', 'L', 'W'},
+               {'L', 'W', 'W', 'L', 'L'},
+               {'L', 'L', 'W', 'W', 'W'},};
+    for(int row = 0; row<islands.size(); row++){
+        for(int col = 0; col<islands[0].size(); col++){
+            count += explore(islands, row, col, visited);
+        }
+    }
+    cout<<count;
     return 0;
 }
